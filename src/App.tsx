37c43ccb9abf0029b384d21666fe44ee0af943d2
jacques-twizeyimana <sandbergjacques500@gmail.com>
2024-11-21 14:55:57 +0200

    feat: add a welcome page to flexiblex

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import { Toaster } from 'react-hot-toast';
-import Login from './pages/Login';
-import Signup from './pages/Signup';
-import CompanySetup from './pages/CompanySetup';
-import InviteTeam from './pages/InviteTeam';
-import Dashboard from './pages/Dashboard';
+import React from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import CompanySetup from "./pages/CompanySetup";
+import InviteTeam from "./pages/InviteTeam";
+import Dashboard from "./pages/Dashboard";
+import Home from "./pages/Home";
 
 function App() {
   return (
@@ -17,7 +18,7 @@ function App() {
           <Route path="/company-setup" element={<CompanySetup />} />
           <Route path="/invite-team" element={<InviteTeam />} />
           <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/" element={<Navigate to="/login" replace />} />
+          <Route path="/" element={<Home />} />
         </Routes>
         <Toaster position="top-center" />
       </div>
